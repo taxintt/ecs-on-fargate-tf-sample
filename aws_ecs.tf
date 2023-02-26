cd15226f759a67dc0a2eb2dae611400bf5a7b343
taxintt <nthouse14@gmail.com>
2023-02-26 22:03:49 +0900

    fix: modify target_group_arn to aws_lb_target_group.frontend.arn

--- aws_ecs.tf
+++ aws_ecs.tf
@@ -41,7 +41,7 @@ resource "aws_ecs_service" "frontend" {
   load_balancer {
     # INFO: ARN of the Load Balancer target group to associate with the service.
     # TODO: fix me and follow the content of book
-    target_group_arn = aws_lb_target_group.green.arn
+    target_group_arn = aws_lb_target_group.frontend.arn
     container_name   = "app"
     container_port   = 80
   }
