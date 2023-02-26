0fc506bff3cc8a6858606142ff1287705c89e7e3
taxintt <nthouse14@gmail.com>
2023-02-26 20:44:35 +0900

    feat: implement ecs stack and test terraform apply/destroy

--- aws_ecs.tf
+++ aws_ecs.tf
@@ -1,92 +1,136 @@
-# resource "aws_kms_key" "example" {
-#   description             = "example"
-#   deletion_window_in_days = 7
-# }
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_cluster
+resource "aws_ecs_cluster" "backend" {
+  name = "sbcntr-ecs-backend-cluster"
 
-# resource "aws_cloudwatch_log_group" "example" {
-#   name = "example"
-# }
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
 
-# resource "aws_ecs_cluster" "test" {
-#   name = "example-cluster"
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
+resource "aws_ecs_service" "backend" {
+  name            = "sbcntr-ecs-backend-service"
+  cluster         = aws_ecs_cluster.backend.id
+  task_definition = aws_ecs_task_definition.backend.arn
+  desired_count   = 2
 
-#   configuration {
-#     execute_command_configuration {
-#       kms_key_id = aws_kms_key.example.arn
-#       logging    = "OVERRIDE"
+  launch_type = "FARGATE"
 
-#       log_configuration {
-#         cloud_watch_encryption_enabled = true
-#         cloud_watch_log_group_name     = aws_cloudwatch_log_group.example.name
-#       }
-#     }
-#   }
-# }
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/platform-linux-fargate.html
+  platform_version = "1.4.0"
 
-# resource "aws_ecs_service" "example" {
-#   name            = "example-app"
-#   cluster         = aws_ecs_cluster.test.id
-#   task_definition = aws_ecs_task_definition.service.arn
-#   desired_count   = 3
-#   iam_role        = aws_iam_role.foo.arn
-#   depends_on      = [aws_iam_role_policy.foo]
+  # INFO: work 1 task at least and work 2 tasks at most
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
 
-#   ordered_placement_strategy {
-#     type  = "binpack"
-#     field = "cpu"
-#   }
+  enable_ecs_managed_tags = true
+  network_configuration {
+    security_groups = [aws_security_group.internal.id]
+    subnets = [
+      aws_subnet.application_1a.id,
+      aws_subnet.application_1c.id,
+    ]
+    assign_public_ip = false
+  }
 
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.foo.arn
-#     container_name   = "mongo"
-#     container_port   = 8080
-#   }
+  health_check_grace_period_seconds = 120
+  load_balancer {
+    # INFO: ARN of the Load Balancer target group to associate with the service.
+    # TODO: fix me and follow the content of book
+    target_group_arn = aws_lb_target_group.green.arn
+    container_name   = "app"
+    container_port   = 80
+  }
 
-#   placement_constraints {
-#     type       = "memberOf"
-#     expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-#   }
-# }
+  deployment_circuit_breaker {
+    enable = false
+    rollback = false
+  }
 
-# # TODO
-# resource "aws_ecs_task_definition" "service" {
-#   family = "service"
-#   container_definitions = jsonencode([
-#     {
-#       name      = "first"
-#       image     = "service-first"
-#       cpu       = 10
-#       memory    = 512
-#       essential = true
-#       portMappings = [
-#         {
-#           containerPort = 80
-#           hostPort      = 80
-#         }
-#       ]
-#     },
-#     {
-#       name      = "second"
-#       image     = "service-second"
-#       cpu       = 10
-#       memory    = 256
-#       essential = true
-#       portMappings = [
-#         {
-#           containerPort = 443
-#           hostPort      = 443
-#         }
-#       ]
-#     }
-#   ])
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
 
-#   volume {
-#     name      = "service-storage"
-#     host_path = "/ecs/service-storage"
-#   }
+  service_registries {
+    registry_arn = aws_service_discovery_service.backend.arn
+  }
 
-#   placement_constraints {
-#     type       = "memberOf"
-#     expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-#   }
-# }
+  lifecycle {
+    ignore_changes = [task_definition, load_balancer]
+  }
+}
+
+# service registry
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/service_discovery_service.html
+resource "aws_service_discovery_service" "backend" {
+  name = "sbcntr-ecs-backend-service"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.local.id
+
+    dns_records {
+      ttl  = 60
+      type = "A"
+    }
+
+    # TODO: check pros of "MULTIVALUE"
+    # see https://christina04.hatenablog.com/entry/ecs-service-discovery
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/service_discovery_private_dns_namespace
+resource "aws_service_discovery_private_dns_namespace" "local" {
+  name = "local"
+  vpc  = aws_vpc.main.id
+}
+
+# ecs task definition
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_task_definition
+resource "aws_ecs_task_definition" "backend" {
+  family                   = "sbcntr-backend-def"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  cpu    = 512
+  memory = 1024
+
+  # INFO: execution_role_arn is not specified in default setting
+  # execution_role_arn = "xxx"
+
+  # ref: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html
+  container_definitions = jsonencode([
+    {
+      name = "app"
+
+      # INFO: pull docker hub image from ECR
+      # https://aws.amazon.com/jp/blogs/containers/docker-official-images-now-available-on-amazon-elastic-container-registry-public/
+      # https://gallery.ecr.aws/nginx/nginx
+      image = "public.ecr.aws/nginx/nginx:alpine"
+
+      # TODO: if developer needs to use private repo, configure the below section
+      # see https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/private-auth.html
+      # "repositoryCredentials": {
+      #       "credentialsParameter": "arn:aws:secretsmanager:region:aws_account_id:secret:secret_name"
+      # }
+      cpu       = 2
+      memory    = 512
+      essential = true
+
+      # INFO: used for ECS exec
+      # see https://toris.io/2021/06/using-ecs-exec-with-readonlyrootfilesystem-enabled-containers/
+      readonlyRootFilesystem = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
