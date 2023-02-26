2ad5cff3b21b8e4a69475132d091616c90326fba
taxintt <nthouse14@gmail.com>
2023-02-26 23:41:10 +0900

    fix: modify resource names

--- aws_iam.tf
+++ aws_iam.tf
@@ -36,12 +36,12 @@ data "aws_iam_policy_document" "ecs_tasks_execution_role" {
   }
 }
 
-resource "aws_iam_role" "ecs_tasks_execution_role" {
+resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "ecs-task-execution-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_execution_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
-  role       = aws_iam_role.ecs_tasks_execution_role.name
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
