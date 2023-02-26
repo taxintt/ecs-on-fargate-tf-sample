aa648a8715f467b064cfdca567c95595af4917d2
taxintt <nthouse14@gmail.com>
2023-02-27 01:11:31 +0900

    fix: add AmazonEC2ContainerRegistryReadOnly policy to ecs task execution role to pull image from ecr

--- aws_iam.tf
+++ aws_iam.tf
@@ -41,7 +41,12 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_execution_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_readonly_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerRegistryReadOnly"
 }
