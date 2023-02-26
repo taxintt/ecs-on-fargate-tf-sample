f0543ebce8cdb9fc4e24ce0595fcd6b07a1c85dc
taxintt <nthouse14@gmail.com>
2023-02-26 18:19:06 +0900

    feat: add escCodeDeployRole role to use code deploy for ECS

---
+++ aws_iam.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "codedeploy" {
+  name        = "escCodeDeployRole"
+  description = "Allows CodeDeploy to read S3 objects..."
+
+  assume_role_policy = data.aws_iam_policy_document.codedeploy.json
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "codedeploy" {
+  role       = aws_iam_role.codedeploy.id
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
+}
+
+data "aws_iam_policy_document" "codedeploy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
+  }
+}
