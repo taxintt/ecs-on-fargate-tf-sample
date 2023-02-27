e3129fb2f3670872ad27a00c99901711cf215781
taxintt <nthouse14@gmail.com>
2023-02-27 15:31:18 +0900

    feat: add sql for Aurora setup

---
+++ tmp/setup.sql
@@ -0,0 +1,39 @@
+-- mysql -h <write_endpoint> -u admin -p (password: (random string))
+
+-- 1. confirm default user
+select host, user from mysql.user;
+
+-- 2. setup > user
+create user sbcntruser@'%' identified by 'sbcntrEncP';
+grant all on sbcntrapp.* to sbcntruser@'%' with grant option;
+
+create user migrate@'%' identified by 'sbcntrMigrate';
+grant all on sbcntrapp.* to migrate@'%' with grant option;
+grant all on `prisma_migrate_shadow_db%`.* to migrate@'%' with grant option;
+
+select host, user from mysql.user;
+
+-- 3. setup > table and initial data
+-- mysql -h <write_endpoint> -u sbcntruser -p 
+-- password : sbcntrEncP
+-- exit;
+-- mysql -h <write_endpoint> -u migrate -p
+-- password : sbcntrMigrate
+-- exit;
+use sbcntrapp;
+show tables;
+
+-- in cloud9 w/ sbcntr-frontend
+-- git checkout main
+-- export DB_USERNAME=migrate
+-- export DB_PASSQORD=sbcntrMigrate
+-- export DB_HOST=<write_endpoint>
+-- export DB_NAME=sbcntrapp
+-- npm run migrate:dev
+-- npm run seed
+
+-- 4. confirm initial data
+-- mysql -h <write_endpoint> -u sbcntruser -p 
+use sbcntrapp;
+show tables;
+select * from Notification;
