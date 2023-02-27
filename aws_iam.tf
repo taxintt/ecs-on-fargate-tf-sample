ad128ada7c1c78398ee79bc4ac708e09d219818f
taxintt <nthouse14@gmail.com>
2023-02-27 21:05:15 +0900

    fix: add comment

--- aws_iam.tf
+++ aws_iam.tf
@@ -47,6 +47,7 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# INFO: role dependencies
 resource "aws_iam_role_policy_attachment" "ecr_readonly_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerRegistryReadOnly"
--- aws_vpc.tf
+++ aws_vpc.tf
@@ -145,6 +145,7 @@ resource "aws_internet_gateway" "main" {
 
 # internet gateway attachment
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway_attachment
+# INFO: aws_internet_gateway dependencies
 resource "aws_internet_gateway_attachment" "main" {
   internet_gateway_id = aws_internet_gateway.main.id
   vpc_id              = aws_vpc.main.id
