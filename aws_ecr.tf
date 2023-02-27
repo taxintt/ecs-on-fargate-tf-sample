be31938faeddf8ed118cfce2d66c2702a2b30ec6
taxintt <nthouse14@gmail.com>
2023-02-27 11:03:55 +0900

    fix: create ECR repository manually before terraform apply

--- aws_ecr.tf
+++ aws_ecr.tf
@@ -1,29 +1,30 @@
 # aws_ecr_repository
+# INFO: create ECR manually to pull images by ecs tasks
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_repository
-resource "aws_ecr_repository" "sbcntr_frontend" {
-  name                 = "sbcntr-frontend"
-  image_tag_mutability = "MUTABLE"
+# resource "aws_ecr_repository" "sbcntr_frontend" {
+#   name                 = "sbcntr-frontend"
+#   image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = false
-  }
+#   image_scanning_configuration {
+#     scan_on_push = false
+#   }
 
-  encryption_configuration {
-    encryption_type = "KMS"
-    # INFO: KMS default key is used if not specified
-  }
-}
+#   encryption_configuration {
+#     encryption_type = "KMS"
+#     # INFO: KMS default key is used if not specified
+#   }
+# }
 
-resource "aws_ecr_repository" "sbcntr_backend" {
-  name                 = "sbcntr-backend"
-  image_tag_mutability = "MUTABLE"
+# resource "aws_ecr_repository" "sbcntr_backend" {
+#   name                 = "sbcntr-backend"
+#   image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = false
-  }
+#   image_scanning_configuration {
+#     scan_on_push = false
+#   }
 
-  encryption_configuration {
-    encryption_type = "KMS"
-    # INFO: KMS default key is used if not specified
-  }
-}
+#   encryption_configuration {
+#     encryption_type = "KMS"
+#     # INFO: KMS default key is used if not specified
+#   }
+# }
